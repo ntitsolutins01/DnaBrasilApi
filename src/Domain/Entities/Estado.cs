159aaa59d8052bc3147c079f16c7ba42b5f4c1c3
Victor Concentino Dev <victordev.net@outlook.com>
2023-10-06 09:37:55 -0300

    chore: inclusão de Estados e Municipios

---
+++ src/Domain/Entities/Estado.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnaBrasil.Domain.Entities;
+public class Estado
+{
+    public string? Uf { get; set; }
+    public string? Sigla { get; set;}
+    public string? Nome { get; set; }
+}
---
+++ src/Infrastructure/Data/Configurations/EstadosConfigurations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DnaBrasil.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace DnaBrasil.Infrastructure.Data.Configurations;
+public class EstadosConfigurations : IEntityTypeConfiguration<Estado>
+{
+    public void Configure(EntityTypeBuilder<Estado> builder)
+    {
+        builder.Property(t => t.Uf)
+            .HasMaxLength(50)
+            .IsRequired();
+        builder.Property(t => t.Sigla)
+            .HasMaxLength(2)
+            .IsRequired();
+        builder.Property(t => t.Nome)
+            .HasMaxLength(150)
+            .IsRequired();
+    }
+}
