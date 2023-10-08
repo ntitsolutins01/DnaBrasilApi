0385493c242fd0a20b6c007a38a16e4b178f0f3e
Victor Concentino Dev <victordev.net@outlook.com>
2023-10-08 17:04:39 -0300

    chore: criacao tabela Vocacional

---
+++ src/Domain/Entities/Vocacional.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnaBrasil.Domain.Entities;
+public class Vocacional : BaseAuditableEntity
+{
+    public required Profissional Profissional { get; set; }
+    public string? Descricao { get; set; }
+    public string? Resposta { get; set; }
+}
