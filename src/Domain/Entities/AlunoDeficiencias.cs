e96c9df6f0e5dd283fbd1e46d3b1eb53472e950e
Victor Concentino Dev <victordev.net@outlook.com>
2023-10-10 11:19:22 -0300

    chore: criacao tabela AlunoDeficiencia

---
+++ src/Domain/Entities/AlunoDeficiencias.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnaBrasil.Domain.Entities;
+public class AlunoDeficiencias : BaseAuditableEntity
+{
+    public string? Deficiencia { get; set; }
+}
