bd0f4dc82e295d2971441c683fb589344c28c553
ntitsolutins01 <ntitsolutions01@gmail.com>
2023-10-20 23:49:39 -0300

    fix: coluna habilitado

--- src/Domain/Entities/Profissional.cs
+++ src/Domain/Entities/Profissional.cs
@@ -16,12 +16,12 @@ public class Profissional : BaseAuditableEntity
     public string? Telefone { get; set; }
     public string? Celular { get; set; }
     public string? Endereco { get; set; }
-    public int Numero { get; set; }
+    public int? Numero { get; set; }
     public string? Cep { get; set; }
     public string? Bairro { get; set; }
     public bool Status { get; set; } = true;
-    public bool? Habilitado { get; set; }
     public Municipio? Municipio { get; set; }
     public List<Ambiente>? Ambientes { get; init; } = new();
+    public bool? Habilitado { get; set; }
 
 }
