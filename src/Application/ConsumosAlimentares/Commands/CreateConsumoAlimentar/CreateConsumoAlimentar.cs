413c3a80f41b2c36e8435f36796047f8d1d2e69f
FabioMuniz <ntitsolutions01@gmail.com>
2023-10-31 16:35:46 -0300

    chore: consumo alimentar create

---
+++ src/Application/ConsumosAlimentares/Commands/CreateConsumoAlimentar/CreateConsumoAlimentar.cs
@@ -0,0 +1,39 @@
+﻿using DnaBrasil.Application.Common.Interfaces;
+using DnaBrasil.Domain.Entities;
+
+namespace DnaBrasil.Application.ConsumosAlimentares.Commands.CreateConsumoAlimentar;
+
+public record CreateConsumoAlimentarCommand : IRequest<int>
+{
+    public required Profissional Profissional { get; set; }
+    public required List<Questionario> Questionarios { get; set; }
+    public required string Resposta { get; set; }
+    public required Aluno Aluno { get; set; }
+}
+
+public class CreateConsumoAlimentarCommandHandler : IRequestHandler<CreateConsumoAlimentarCommand, int>
+{
+    private readonly IApplicationDbContext _context;
+
+    public CreateConsumoAlimentarCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> Handle(CreateConsumoAlimentarCommand request, CancellationToken cancellationToken)
+    {
+        var entity = new ConsumoAlimentar
+        {
+            Profissional = request.Profissional,
+            Questionarios = request.Questionarios,
+            Resposta = request.Resposta,
+            Aluno = request.Aluno
+        };
+
+        _context.ConsumoAlimentares.Add(entity);
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return entity.Id;
+    }
+}
