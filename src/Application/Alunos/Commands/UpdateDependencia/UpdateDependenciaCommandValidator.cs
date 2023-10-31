0db0b93c82f990721a803cd65189d4d8ac5fa803
Victor Concentino Dev <victordev.net@outlook.com>
2023-10-31 13:27:14 -0300

    chore: implementacao commandValidator de Create e Update Dependencia

--- src/Application/Alunos/Commands/UpdateDependencia/UpdateDependenciaCommandValidator.cs
+++ src/Application/Alunos/Commands/UpdateDependencia/UpdateDependenciaCommandValidator.cs
@@ -10,6 +10,33 @@ public class UpdateDependenciaCommandValidator : AbstractValidator<UpdateDepende
     {
         _context = context;
 
-        
+        RuleFor(v => v.Doencas)
+            .MaximumLength(150)
+            .NotEmpty();
+        RuleFor(v => v.Nacionalidade)
+            .MaximumLength(150)
+            .NotEmpty();
+        RuleFor(v => v.Naturalidade)
+            .MaximumLength(150)
+            .NotEmpty();
+        RuleFor(v => v.NomeEscola)
+            .MaximumLength(150)
+            .NotEmpty();
+        RuleFor(v => v.TipoEscola)
+            .NotEmpty();
+        RuleFor(v => v.TipoEscolaridade)
+            .NotEmpty();
+        RuleFor(v => v.Turno)
+            .MaximumLength(150)
+            .NotEmpty();
+        RuleFor(v => v.Serie)
+            .MaximumLength(150)
+            .NotEmpty();
+        RuleFor(v => v.Ano)
+            .MaximumLength(150)
+            .NotEmpty();
+        RuleFor(v => v.Turma)
+            .MaximumLength(150)
+            .NotEmpty();
     }
 }
