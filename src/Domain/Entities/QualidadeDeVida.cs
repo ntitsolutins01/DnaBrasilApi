5795eeebed726dd0b748e42827879ebe4b450378
FabioMuniz <ntitsolutions01@gmail.com>
2023-10-31 20:54:41 -0300

    chore: migration Qualidade de Vida

--- src/Domain/Entities/QualidadeDeVida.cs
+++ src/Domain/Entities/QualidadeDeVida.cs
@@ -8,8 +8,6 @@ namespace DnaBrasil.Domain.Entities;
 public class QualidadeDeVida : BaseAuditableEntity
 {
     public required Profissional Profissional { get; set; }
-    public string? Descricao { get; set; }
-    public string? Resposta { get; set; }
-    public required Aluno Aluno { get; set; }
-    public required List<Questionario> Questionarios { get; set; }
+    public required Questionario Questionario { get; set; }
+    public required string Resposta { get; set; }
 }
