f0e266aaf06d5fd5b60d79b28e5e3c231dae93cf
Victor Concentino Dev <victordev.net@outlook.com>
2023-11-02 20:03:30 -0300

    chore: implementação CQRS e CRUD de Serie em Application

---
+++ src/Application/Series/Commands/DeleteSerie/DeleteSerie.cs
@@ -0,0 +1,29 @@
+﻿using DnaBrasil.Application.Common.Interfaces;
+
+namespace DnaBrasil.Application.Series.Commands.DeleteSerie;
+public record DeleteSerieCommand(int Id) : IRequest;
+
+public class DeleteSerieCommandHandler : IRequestHandler<DeleteSerieCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public DeleteSerieCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Handle(DeleteSerieCommand request, CancellationToken cancellationToken)
+    {
+        var entity = await _context.Series
+            .FindAsync(new object[] { request.Id }, cancellationToken);
+
+        Guard.Against.NotFound(request.Id, entity);
+
+        _context.Series.Remove(entity);
+
+        //entity.AddDomainEvent(new SerieDeletedEvent(entity));
+
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+
+}
