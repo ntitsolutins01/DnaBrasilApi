8840863126bec32222d2ea8fa19db24969fb9a12
DRKinfo <126280960+DRKinfo@users.noreply.github.com>
2023-11-07 21:41:06 -0300

    Chore: Implementacao de tabela escolaridade

---
+++ src/Domain/Entities/Escolaridade.cs
@@ -0,0 +1,7 @@
+﻿namespace DnaBrasil.Domain.Entities;
+public class Escolaridade : BaseAuditableEntity
+{
+    public required string Nome { get; set; }
+    public bool Status { get; set; } = true;
+    public string? Descricao { get; set; }
+}
