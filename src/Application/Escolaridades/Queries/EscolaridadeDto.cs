f91412b8f84c2ec881648b8e894b4ff4896fac35
FabioMuniz <ntitsolutions01@gmail.com>
2023-11-16 15:52:03 -0300

    feat: implementa'cao de application de escolaridade

---
+++ src/Application/Escolaridades/Queries/EscolaridadeDto.cs
@@ -0,0 +1,19 @@
+﻿using DnaBrasil.Application.Alunos.Queries;
+using DnaBrasil.Application.Profissionais.Queries;
+using DnaBrasil.Domain.Entities;
+
+namespace DnaBrasil.Application.Escolaridades.Queries;
+public class EscolaridadeDto
+{
+    public string? Nome { get; set; }
+    public bool Status { get; set; }
+    public List<AlunoDto> Alunos { get; } = new();
+    public List<ProfissionalDto> Profissionais { get; } = new();
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<Escolaridade, EscolaridadeDto>();
+        }
+    }
+}
---
+++ src/Application/Escolaridades/Queries/GetEscolaridadesAll/GetEscolaridadesAll.cs
@@ -0,0 +1,28 @@
+﻿using DnaBrasil.Application.Common.Interfaces;
+
+namespace DnaBrasil.Application.Ambientes.Queries.GetAmbientesAll;
+//[Authorize]
+public record GetAmbientesQuery : IRequest<List<AmbienteDto>>;
+
+public class GetAmbientesQueryHandler : IRequestHandler<GetAmbientesQuery, List<AmbienteDto>>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetAmbientesQueryHandler(IApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<List<AmbienteDto>> Handle(GetAmbientesQuery request, CancellationToken cancellationToken)
+    {
+        var result = await _context.Ambientes
+            .AsNoTracking()
+            .ProjectTo<AmbienteDto>(_mapper.ConfigurationProvider)
+            .OrderBy(t => t.Nome)
+            .ToListAsync(cancellationToken);
+
+        return result == null ? throw new ArgumentNullException(nameof(result)) : result;
+    }
+}
