cb17bd8674ecc129fe4a1bbb99bae89a268c8e81
FabioMuniz <ntitsolutions01@gmail.com>
2023-11-18 12:38:57 -0300

    chore: implementacao de update e delete de usuarios

---
+++ src/Application/Usuarios/Commands/UpdateUsuario/UpdateUsuario.cs
@@ -0,0 +1,38 @@
+﻿using DnaBrasilApi.Application.Common.Interfaces;
+using DnaBrasilApi.Domain.Entities;
+
+namespace DnaBrasilApi.Application.Usuarios.Commands.UpdateUsuario;
+
+public record UpdateUsuarioCommand : IRequest<bool>
+{
+    public int Id { get; init; }
+    public required string Nome { get; init; }
+    public required string Email { get; init; }
+    public bool Status { get; init; }
+}
+
+public class UpdateUsuarioCommandHandler : IRequestHandler<UpdateUsuarioCommand, bool>
+{
+    private readonly IApplicationDbContext _context;
+
+    public UpdateUsuarioCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> Handle(UpdateUsuarioCommand request, CancellationToken cancellationToken)
+    {
+        var entity = await _context.Usuarios
+            .FindAsync(new object[] { request.Id }, cancellationToken);
+
+        Guard.Against.NotFound(request.Id, entity);
+
+        entity.Nome = request.Nome;
+        entity.Email = request.Email;
+        entity.Status = request.Status;
+
+        var result = await _context.SaveChangesAsync(cancellationToken);
+
+        return result == 1;//true
+    }
+}
---
+++ src/Application/Usuarios/Commands/UpdateUsuario/UpdateUsuarioCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿namespace DnaBrasilApi.Application.Usuarios.Commands.UpdateUsuario;
+
+public class UpdateUsuarioCommandValidator : AbstractValidator<UpdateUsuarioCommand>
+{
+    public UpdateUsuarioCommandValidator()
+    {
+        RuleFor(v => v.Nome)
+            .MaximumLength(150)
+            .NotNull().NotEmpty();
+        RuleFor(v => v.Email)
+            .MaximumLength(100)
+            .NotNull().NotEmpty();
+
+        RuleFor(v => v.Status)
+            .NotNull().NotEmpty();
+    }
+}
