65941e43900872627bc80769a32cc8101d162279
FabioMuniz <ntitsolutions01@gmail.com>
2023-11-18 11:54:11 -0300

    chore: inclusao de novos endpoints para

--- src/Web/Endpoints/Usuarios.cs
+++ src/Web/Endpoints/Usuarios.cs
@@ -1,4 +1,6 @@
-﻿using DnaBrasilApi.Application.Usuarios.Commands.CreateUsuario;
+﻿using DnaBrasilApi.Application.Usuarios.Commands.DeleteUsuario;
+using DnaBrasilApi.Application.Usuarios.Commands.UpdateUsuario;
+using DnaBrasilApi.Application.Usuarios.Commands.CreateUsuario;
 using DnaBrasilApi.Application.Usuarios.Queries;
 
 namespace DnaBrasilApi.Web.Endpoints;
@@ -10,19 +12,43 @@ public class Usuarios : EndpointGroupBase
         app.MapGroup(this)
             //.RequireAuthorization()
             .MapGet(GetUsuariosAll)
+            .MapGet(GetUsuarioById, "Usuario/{id}")
             .MapPost(CreateUsuario)
+            .MapPut(UpdateUsuario, "{id}")
+            .MapDelete(DeleteUsuario, "{id}")
             .MapGet(GetUsuarioByEmail, "Email/{email}")
             .MapGet(GetUsuarioByCpf, "Cpf/{cpf}");
     }
 
-    public async Task<List<UsuarioDto>> GetUsuariosAll(ISender sender)
-    {
-        return await sender.Send(new GetUsuariosAllQuery());
-    }
+    #region Main Methods
 
     public async Task<int> CreateUsuario(ISender sender, CreateUsuarioCommand command)
     {
         return await sender.Send(command);
+    }
+
+    public async Task<bool> UpdateUsuario(ISender sender, int id, UpdateUsuarioCommand command)
+    {
+        if (id != command.Id) return false;
+        var result = await sender.Send(command);
+        return result;
+    }
+
+    public async Task<bool> DeleteUsuario(ISender sender, int id)
+    {
+        return await sender.Send(new DeleteUsuarioCommand(id));
+    }
+
+    #endregion
+    #region Methods
+
+    public async Task<UsuarioDto> GetUsuarioById(ISender sender, int id)
+    {
+        return await sender.Send(new GetUsuarioByIdQuery() { Id = id });
+    }
+    public async Task<List<UsuarioDto>> GetUsuariosAll(ISender sender)
+    {
+        return await sender.Send(new GetUsuariosAllQuery());
     }
     public async Task<UsuarioDto> GetUsuarioByEmail(ISender sender, string email)
     {
@@ -32,4 +58,6 @@ public class Usuarios : EndpointGroupBase
     {
         return await sender.Send(new GetUsuarioByCpfQuery() { Cpf = cpf });
     }
+
+    #endregion 
 }
