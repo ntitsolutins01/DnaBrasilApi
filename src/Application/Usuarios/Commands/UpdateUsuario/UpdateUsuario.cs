ea0f4cbd4cd5d410a89ca1a71ba96097f163a5cb
FabioMuniz <ntitsolutions01@gmail.com>
2023-11-18 12:42:19 -0300

    chore: inclusao de atualizacao de perfil no momento do update de usuario

--- src/Application/Usuarios/Commands/UpdateUsuario/UpdateUsuario.cs
+++ src/Application/Usuarios/Commands/UpdateUsuario/UpdateUsuario.cs
@@ -8,6 +8,7 @@ public record UpdateUsuarioCommand : IRequest<bool>
     public int Id { get; init; }
     public required string Nome { get; init; }
     public required string Email { get; init; }
+    public int PerfilId { get; init; }
     public bool Status { get; init; }
 }
 
@@ -27,9 +28,12 @@ public class UpdateUsuarioCommandHandler : IRequestHandler<UpdateUsuarioCommand,
 
         Guard.Against.NotFound(request.Id, entity);
 
+        var perfil = await _context.Perfis.Where(x => x.Id == request.PerfilId).FirstOrDefaultAsync();
+
         entity.Nome = request.Nome;
         entity.Email = request.Email;
         entity.Status = request.Status;
+        entity.Perfil = perfil;
 
         var result = await _context.SaveChangesAsync(cancellationToken);
 
