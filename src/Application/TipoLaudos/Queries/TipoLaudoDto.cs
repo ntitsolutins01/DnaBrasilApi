9edcfd3a2b1303c32d9bef08d6bc8f8140a913ef
FabioMuniz <ntitsolutions01@gmail.com>
2023-11-30 19:08:41 -0300

    fix: entidade

--- src/Application/TipoLaudos/Queries/TipoLaudoDto.cs
+++ src/Application/TipoLaudos/Queries/TipoLaudoDto.cs
@@ -7,9 +7,6 @@ public class TipoLaudoDto
     public int Id { get; init; }
     public string? Nome { get; init; }
     public string? Descricao { get; init; }
-    public int IdadeInicial { get; init; }
-    public int IdadeFinal { get; init; }
-    public int ScoreTotal { get; init; }
 
     private class Mapping : Profile
     {
