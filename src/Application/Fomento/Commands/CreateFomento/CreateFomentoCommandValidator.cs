512f8ecea16fd64e0dfcb8b9abc00fe9064834ae
drkinfo616@gmail.com <drkinfo616@gmail.com>
2023-12-04 15:38:56 -0300

    Auto stash before merge of "feature/nova-release" and "origin/feature/nova-release"

--- src/Application/Fomento/Commands/CreateFomento/CreateFomentoCommandValidator.cs
+++ src/Application/Fomento/Commands/CreateFomento/CreateFomentoCommandValidator.cs
@@ -6,24 +6,9 @@ internal class CreateFomentoCommandValidator : AbstractValidator<CreateFomentoCo
         RuleFor(v => v.Nome)
             .MaximumLength(150)
             .NotEmpty();
-        RuleFor(v => v.Descricao)
-            .MaximumLength(200)
-            .NotEmpty();
-        RuleFor(v => v.Descricao)
-            .MaximumLength(200)
-            .NotEmpty();
-        RuleFor(v => v.IdadeInicial)
-            .InclusiveBetween(5, 18)
-            .WithMessage("'{PropertyName}' deve ser maior 5 e menor que 18.")
-            .NotEmpty();
-        RuleFor(v => v.IdadeFinal)
-            .InclusiveBetween(5, 18)
-            .WithMessage("'{PropertyName}' deve ser maior 5 e menor que 18.")
-            .NotEmpty();
-        RuleFor(v => v.ScoreTotal)
-            .InclusiveBetween(0, 1000)
-            .WithMessage("'{PropertyName}' deve ser maior 0 e menor que 1000.")
-            .NotEmpty();
-
+        RuleFor(v => v.MunicipioId)
+            .NotNull();
+        RuleFor(v => v.LocalidadeId)
+            .NotNull();
     }
 }
