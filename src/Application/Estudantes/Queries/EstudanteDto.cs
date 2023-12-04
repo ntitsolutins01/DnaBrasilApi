297c5c0a7720188046269eb56f3045a348662ff5
FabioMuniz <ntitsolutions01@gmail.com>
2023-12-04 11:19:44 -0300

    chore: crud estudante

---
+++ src/Application/Estudantes/Queries/EstudanteDto.cs
@@ -0,0 +1,17 @@
+﻿using DnaBrasilApi.Application.Ambientes.Queries;
+using DnaBrasilApi.Application.Contratos.Queries;
+using DnaBrasilApi.Application.Municipios.Queries;
+using DnaBrasilApi.Domain.Entities;
+
+namespace DnaBrasilApi.Application.Estudantes.Queries;
+public class EstudanteDto
+{
+    public int Id { get; set; }
+    //private class Mapping : Profile
+    //{
+    //    public Mapping()
+    //    {
+    //        CreateMap<Estudante, EstudanteDto>();
+    //    }
+    //}
+}
