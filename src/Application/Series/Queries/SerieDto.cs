9634da1ebfeb597836e4880713f6c212b3a2ccf5
FabioMuniz <ntitsolutions01@gmail.com>
2023-12-12 23:17:50 -0300

    fix: campos faltantes

--- src/Application/Series/Queries/SerieDto.cs
+++ src/Application/Series/Queries/SerieDto.cs
@@ -6,6 +6,9 @@ public class SerieDto
     public int Id { get; init; }
     public string? Nome { get; init; }
     public string? Descricao { get; init; }
+    public  int IdadeInicial { get; set; }
+    public  int IdadeFinal { get; set; }
+    public  int ScoreTotal { get; set; }
     public bool Status { get; set; }
 
     private class Mapping : Profile
