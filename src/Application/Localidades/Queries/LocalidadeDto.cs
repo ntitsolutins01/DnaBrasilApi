fd400a954e039ae89d7386871d357f0136c1da2b
FabioMuniz <ntitsolutions01@gmail.com>
2024-01-02 11:48:40 -0300

    chore: ajuste de dto de localidade

--- src/Application/Localidades/Queries/LocalidadeDto.cs
+++ src/Application/Localidades/Queries/LocalidadeDto.cs
@@ -1,5 +1,4 @@
-﻿using DnaBrasilApi.Application.Municipios.Queries;
-using DnaBrasilApi.Domain.Entities;
+﻿using DnaBrasilApi.Domain.Entities;
 
 namespace DnaBrasilApi.Application.Localidades.Queries;
 public class LocalidadeDto
@@ -8,12 +7,20 @@ public class LocalidadeDto
     public string? Nome { get; init; }
     public string? Descricao { get; init; }
     public bool Status { get; set; }
+    public int MunicipioId { get; set; }
+    public int EstadoId { get; set; }
+    public string? NomeMunicipio { get; set; }
+    public string? NomeEstado { get; set; }
 
     private class Mapping : Profile
     {
         public Mapping()
         {
-            CreateMap<Localidade, LocalidadeDto>();
+            CreateMap<Localidade, LocalidadeDto>()
+                .ForMember(dest => dest.MunicipioId, opt => opt.MapFrom(src => src.Municipio!.Id))
+                .ForMember(dest => dest.EstadoId, opt => opt.MapFrom(src => src.Municipio!.Estado!.Id))
+                .ForMember(dest => dest.NomeMunicipio, opt => opt.MapFrom(src => src.Municipio!.Nome))
+                .ForMember(dest => dest.NomeEstado, opt => opt.MapFrom(src => src.Municipio!.Estado!.Nome));
         }
     }
 }
