b5226f0ff30319249f0a135b148e639f2ca05707
DRKinfo <126280960+DRKinfo@users.noreply.github.com>
2024-01-03 10:28:19 -0300

    chore:implementação de parceiro

--- src/Application/Parceiros/Commands/CreateParceiro/CreateParceiro.cs
+++ src/Application/Parceiros/Commands/CreateParceiro/CreateParceiro.cs
@@ -16,7 +16,7 @@ public record CreateParceiroCommand : IRequest<int>
     public string? Celular { get; set; }
     public string? Cep { get; set; }
     public string? Endereco { get; set; }
-    public int Numero { get; set; }
+    public int? Numero { get; set; }
     public string? Bairro { get; set; }
     public bool Status { get; set; }
     public bool? Habilitado { get; set; }
