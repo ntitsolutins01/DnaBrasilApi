1cf2c4db8fc537f9c5097bab4c4d75939af22bbe
Fabio Muniz <fabio.muniz01@gmail.com>
2024-01-23 09:48:48 -0300

    chore? correcao de datas

--- src/Application/Alunos/Commands/UpdateMatricula/UpdateMatricula.cs
+++ src/Application/Alunos/Commands/UpdateMatricula/UpdateMatricula.cs
@@ -1,4 +1,5 @@
-﻿using DnaBrasilApi.Application.Common.Interfaces;
+﻿using System.Globalization;
+using DnaBrasilApi.Application.Common.Interfaces;
 using DnaBrasilApi.Domain.Entities;
 
 namespace DnaBrasilApi.Application.Alunos.Commands.UpdateMatricula;
@@ -6,8 +7,8 @@ namespace DnaBrasilApi.Application.Alunos.Commands.UpdateMatricula;
 public record UpdateMatriculaCommand : IRequest <bool>
 {
     public int Id { get; set; }
-    public DateTime DtVencimentoParq { get; set; }
-    public DateTime DtVencimentoAtestadoMedico { get; set; }
+    public string? DtVencimentoParq { get; set; }
+    public string?  DtVencimentoAtestadoMedico { get; set; }
     public string? NomeResponsavel1 { get; set; }
     public string? ParentescoResponsavel1 { get; set; }
     public string? CpfResponsavel1 { get; set; }
@@ -17,6 +18,7 @@ public record UpdateMatriculaCommand : IRequest <bool>
     public string? NomeResponsavel3 { get; set; }
     public string? ParentescoResponsavel3 { get; set; }
     public string? CpfResponsavel3 { get; set; }
+    public int AlunoId { get; set; }
 }
 
 public class UpdateMatriculaCommandHandler : IRequestHandler<UpdateMatriculaCommand, bool>
@@ -30,13 +32,18 @@ public class UpdateMatriculaCommandHandler : IRequestHandler<UpdateMatriculaComm
 
     public async Task<bool> Handle(UpdateMatriculaCommand request, CancellationToken cancellationToken)
     {
+        var aluno = await _context.Alunos
+            .FindAsync(new object[] { request.AlunoId }, cancellationToken);
+
+        Guard.Against.NotFound(request.AlunoId, aluno);
+
         var entity = await _context.Matriculas
             .FindAsync(new object[] { request.Id }, cancellationToken);
 
         Guard.Against.NotFound(request.Id, entity);
 
-        entity.DtVencimentoParq = request.DtVencimentoParq;
-        entity.DtVencimentoAtestadoMedico = request.DtVencimentoAtestadoMedico;
+        entity.DtVencimentoParq = DateTime.ParseExact(request.DtVencimentoParq!, "dd/MM/yyyy", CultureInfo.CreateSpecificCulture("pt-BR"));
+        entity.DtVencimentoAtestadoMedico = DateTime.ParseExact(request.DtVencimentoAtestadoMedico!, "dd/MM/yyyy", CultureInfo.CreateSpecificCulture("pt-BR"));
         entity.ParentescoResponsavel1 = request.ParentescoResponsavel1;
         entity.NomeResponsavel1 = request.NomeResponsavel1;
         entity.CpfResponsavel1 = request.CpfResponsavel1;
@@ -46,6 +53,7 @@ public class UpdateMatriculaCommandHandler : IRequestHandler<UpdateMatriculaComm
         entity.NomeResponsavel3 = request.NomeResponsavel3;
         entity.CpfResponsavel3 = request.CpfResponsavel3;
         entity.ParentescoResponsavel3 = request.ParentescoResponsavel3;
+        entity.Aluno = aluno;
 
         var result = await _context.SaveChangesAsync(cancellationToken);
 
