d2866e9247631969ee0893ff96e4257e67caf018
Fabio Muniz <fabio.muniz01@gmail.com>
2024-02-04 16:10:53 -0300

    chore: implementacao de busca de indicadores para dashboard

---
+++ src/Application/Dashboards/Queries/GetAlunosBySexo/GetAlunosBySexo.cs
@@ -0,0 +1,37 @@
+﻿using DnaBrasilApi.Application.Alunos.Queries;
+using DnaBrasilApi.Application.Common.Interfaces;
+
+namespace DnaBrasilApi.Application.Dashboards.Queries.GetAlunosBySexo;
+
+public record GetAlunosBySexoQuery : IRequest<int>
+{
+    public string? Sexo { get; init; }
+}
+
+public class GetAlunosBySexoQueryHandler : IRequestHandler<GetAlunosBySexoQuery, int>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetAlunosBySexoQueryHandler(IApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<int> Handle(GetAlunosBySexoQuery request, CancellationToken cancellationToken)
+    {
+        int result = 0;
+
+        result = string.IsNullOrWhiteSpace(request.Sexo)
+            ? await _context.Alunos
+                .AsNoTracking()
+                .CountAsync(cancellationToken)
+            : await _context.Alunos
+                .Where(x => x.Sexo == request.Sexo)
+                .AsNoTracking()
+                .CountAsync(cancellationToken);
+
+        return result;
+    }
+}
---
+++ src/Web/Endpoints/Dashboards.cs
@@ -0,0 +1,26 @@
+﻿using DnaBrasilApi.Application.Alunos.Queries.GetAlunosAll;
+using DnaBrasilApi.Application.Dashboards.Queries;
+using DnaBrasilApi.Application.Dashboards.Queries.GetAlunosBySexo;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DnaBrasilApi.Web.Endpoints;
+
+public class Dashboards : EndpointGroupBase
+{
+    public override void Map(WebApplication app)
+    {
+        app.MapGroup(this)
+            //.RequireAuthorization()
+            .MapGet(GetIndicadoresByFilter, "Indicadores");
+    }
+
+    public Task<DashboardIndicadoresDto> GetIndicadoresByFilter(ISender sender)
+    {
+        var result = new DashboardIndicadoresDto();
+        result.AlunosCadastrados = sender.Send(new GetAlunosBySexoQuery()).Result;
+        result.CadastrosFemininos = sender.Send(new GetAlunosBySexoQuery() { Sexo = "F" }).Result;
+        result.CadastrosMasculinos = sender.Send(new GetAlunosBySexoQuery() { Sexo = "M" }).Result;
+
+        return Task.FromResult(result);
+    }
+}
