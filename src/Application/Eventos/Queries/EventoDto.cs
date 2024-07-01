729b66cd10d0fad4b52e021761fb7bdcc7135dee
Fabio Muniz <fabio.muniz01@gmail.com>
2024-07-01 19:19:09 -0300

    chore: atualizacao dto evento

--- src/Application/Eventos/Queries/EventoDto.cs
+++ src/Application/Eventos/Queries/EventoDto.cs
@@ -6,6 +6,9 @@ public class EventoDto
 {
     public required int Id { get; set; }
     public required string Localidade { get; set; }
+    public required string EstadoId { get; set; }
+    public required string MunicipioId { get; set; }
+    public required string LocalidadeId { get; set; }
     public required string Titulo { get; set; }
     public string? Descricao { get; set; }
     public required string DataEvento { get; set; }
@@ -17,6 +20,9 @@ public class EventoDto
         public Mapping()
         {
             CreateMap<Evento, EventoDto>()
+                .ForMember(dest => dest.MunicipioId, opt => opt.MapFrom(src => src.Localidade.Municipio!.Id))
+                .ForMember(dest => dest.EstadoId, opt => opt.MapFrom(src => src.Localidade.Municipio!.Estado!.Sigla))
+                .ForMember(dest => dest.LocalidadeId, opt => opt.MapFrom(src => src.Localidade.Id))
                 .ForMember(dest => dest.DataEvento,
                     opt => opt.MapFrom(src => src.DataEvento.ToString("dd/MM/yyyy")))
                 .ForMember(dest => dest.Localidade, opt => opt.MapFrom(src => src.Localidade!.Nome));
