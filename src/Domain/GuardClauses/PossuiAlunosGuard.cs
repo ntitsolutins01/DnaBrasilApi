1afe70c50ba45f0b3683c9d67ad5035c42970844
Fabio Muniz <fabio.muniz01@gmail.com>
2024-07-17 21:32:22 -0300

    chore: update encaminhamento vocacional + custom exception

---
+++ src/Domain/GuardClauses/PossuiAlunosGuard.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.GuardClauses;
+
+namespace DnaBrasilApi.Domain.GuardClauses;
+
+public static class PossuiAlunosGuard
+{
+    public static void PossuiAlunos(this IGuardClause guardClause,bool input)
+    {
+        if (input)
+            throw new ArgumentException("O Prodissional possui alunos vinculados.");
+    }
+}
