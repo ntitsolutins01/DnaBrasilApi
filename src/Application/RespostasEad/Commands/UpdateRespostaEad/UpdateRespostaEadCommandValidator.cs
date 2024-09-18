b5f1c045870fb89da592083195d667b86dd9a711
Fabio Muniz <fabio.muniz01@gmail.com>
2024-09-18 15:19:59 -0300

    chore: migracao

--- src/Application/RespostasEad/Commands/UpdateRespostaEad/UpdateRespostaEadCommandValidator.cs
+++ src/Application/RespostasEad/Commands/UpdateRespostaEad/UpdateRespostaEadCommandValidator.cs
@@ -1,5 +1,5 @@
 ﻿namespace DnaBrasilApi.Application.RespostasEad.Commands.UpdateRespostaEad;
-
+//todo: verificar rules validators
 public class UpdateRespostaEadCommandValidator : AbstractValidator<UpdateRespostaEadCommand>
 {
     public UpdateRespostaEadCommandValidator()
