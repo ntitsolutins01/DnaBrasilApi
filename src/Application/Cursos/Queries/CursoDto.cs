bb0ca3e5d4cc9e736c1badc06f40750f08b16979
Fabio Muniz Developer <fabio.muniz01@gmail.com>
2024-09-28 09:10:12 -0300

    fix: ajustes para create de curso com nome correto

--- src/Application/Cursos/Queries/CursoDto.cs
+++ src/Application/Cursos/Queries/CursoDto.cs
@@ -7,8 +7,8 @@ public class CursoDto
     public required int Id { get; init; }
     public required int TipoCursoId { get; init; }
     public required string TituloTipoCurso { get; init; }
-    public required int UsuarioId { get; init; }
-    public required string NomeProfissional { get; init; }
+    public required int CoordenadorId { get; init; }
+    public required string NomeCoordenador { get; init; }
     public required string Titulo { get; init; }
     public required int CargaHoraria { get; init; }
     public string? Descricao { get; init; }
@@ -21,8 +21,8 @@ public class CursoDto
             CreateMap<Curso, CursoDto>()
                 .ForMember(dest => dest.TipoCursoId, opt => opt.MapFrom(src => src.TipoCurso.Id))
                 .ForMember(dest => dest.TituloTipoCurso, opt => opt.MapFrom(src => src.TipoCurso.Nome))
-                .ForMember(dest => dest.UsuarioId, opt => opt.MapFrom(src => src.Usuario.Id))
-                .ForMember(dest => dest.NomeProfissional, opt => opt.MapFrom(src => src.Usuario.Nome));
+                .ForMember(dest => dest.CoordenadorId, opt => opt.MapFrom(src => src.Usuario.Id))
+                .ForMember(dest => dest.NomeCoordenador, opt => opt.MapFrom(src => src.Usuario.Nome));
         }
     }
 }
