5fef8b13b0776b32691211679f681deab72a8c75
Fabio Muniz Developer <fabio.muniz01@gmail.com>
2024-10-02 20:25:15 -0300

    chore: update de encaminhamento de qualidade de vida

--- src/Application/Laudos/Commands/UpdateQualidadeVida/UpdateEncaminhamentoQualidadeDeVida.cs
+++ src/Application/Laudos/Commands/UpdateQualidadeVida/UpdateEncaminhamentoQualidadeDeVida.cs
@@ -4,7 +4,7 @@ using DnaBrasilApi.Domain.Enums;
 
 namespace DnaBrasilApi.Application.Laudos.Commands.UpdateQualidadeVida;
 
-public record UpdateEncaminhamentoQualidadeDeVidaCommand : IRequest <bool>
+public record UpdateEncaminhamentoQualidadeDeVidaCommand : IRequest<bool>
 {
     public int? AlunoId { get; init; }
 }
@@ -43,10 +43,15 @@ public class UpdateEncaminhamentoQualidadeDeVidaCommandHandler : IRequestHandler
 
         var verificaAlunos = alunos.Select(x => x.Id);
 
-        var laudos = _context.Laudos.Where(x => verificaAlunos.Contains(x.Aluno.Id)).Include(i => i.QualidadeDeVida).Where(x => x.QualidadeDeVida != null)
+        var laudos = _context.Laudos.Where(x => verificaAlunos.Contains(x.Aluno.Id))
+            .Include(i => i.QualidadeDeVida)
             .Include(a => a.Aluno)
             .AsNoTracking()
-            .OrderBy(t => t.QualidadeDeVida!.Id);
+            .OrderByDescending(t => t.QualidadeDeVida!.Id);
+
+        var listQualidadeDeVida = _context.QualidadeDeVidas
+            .AsNoTracking()
+            .OrderByDescending(t => t.Id);
 
         int cont = 0;
         decimal quadrante1;
@@ -57,9 +62,11 @@ public class UpdateEncaminhamentoQualidadeDeVidaCommandHandler : IRequestHandler
         var metricas = _context.TextosLaudos
             .Where(x => x.TipoLaudo.Id == (int)EnumTipoLaudo.QualidadeVida).ToList();
 
-        foreach (var laudo in laudos)
+        var tot = listQualidadeDeVida.Count();
+
+        foreach (var qualidadeDeVida in listQualidadeDeVida)
         {
-            List<int> listRespostas = laudo.QualidadeDeVida!.Respostas.Split(',').Select(item => int.Parse(item)).ToList();
+            List<int> listRespostas = qualidadeDeVida.Respostas.Split(',').Select(item => int.Parse(item)).ToList();
 
             var respostas = _context.Respostas.Where(x => listRespostas.Contains(x.Id)).Include(i => i.Questionario);
 
@@ -86,26 +93,17 @@ public class UpdateEncaminhamentoQualidadeDeVidaCommandHandler : IRequestHandler
                     continue;
                 }
 
-                if (laudo.Aluno.Sexo == "M")
-                {
-                    var parametro = result.Aviso.Split('.').First();
+                var parametro = result.Aviso.Split('.').First();
 
-                    encaminhamento.Add(encaminhamentos.First(x => x.Parametro == parametro).Id);
-                }
-                else
-                {
-                    var parametro = result.Aviso.Split('.').First();
-
-                    encaminhamento.Add(encaminhamentos.First(x => x.Parametro == parametro).Id);
-                }
+                encaminhamento.Add(encaminhamentos.First(x => x.Parametro == parametro).Id);
             }
 
             cont = 0;
 
             var entity = await _context.QualidadeDeVidas
-                .FindAsync([laudo.QualidadeDeVida.Id], cancellationToken);
+                .FindAsync([qualidadeDeVida.Id], cancellationToken);
 
-            Guard.Against.NotFound(laudo.QualidadeDeVida.Id, entity);
+            Guard.Against.NotFound(qualidadeDeVida.Id, entity);
 
             entity.Encaminhamentos = string.Join(",", encaminhamento); ;
 
