5ea702a30e418485adf0434f186cc6eb0bfcbd22
Lutercio Jackson <luterciofilho11@hotmail.com>
2024-11-26 18:58:19 -0300

    chore: Delete Certificado

--- src/Application/Certificados/Commands/DeleteCertificado/DeleteCertificado.cs
+++ src/Application/Certificados/Commands/DeleteCertificado/DeleteCertificado.cs
@@ -1,25 +1,25 @@
 ﻿using DnaBrasilApi.Application.Common.Interfaces;
 
-namespace DnaBrasilApi.Application.Aulas.Commands.DeleteAula;
-public record DeleteAulaCommand(int Id) : IRequest<bool>;
+namespace DnaBrasilApi.Application.Certificados.Commands.DeleteCertificado;
+public record DeleteCertificadoCommand(int Id) : IRequest<bool>;
 
-public class DeleteAulaCommandHandler : IRequestHandler<DeleteAulaCommand, bool>
+public class DeleteCertificadoCommandHandler : IRequestHandler<DeleteCertificadoCommand, bool>
 {
     private readonly IApplicationDbContext _context;
 
-    public DeleteAulaCommandHandler(IApplicationDbContext context)
+    public DeleteCertificadoCommandHandler(IApplicationDbContext context)
     {
         _context = context;
     }
 
-    public async Task<bool> Handle(DeleteAulaCommand request, CancellationToken cancellationToken)
+    public async Task<bool> Handle(DeleteCertificadoCommand request, CancellationToken cancellationToken)
     {
-        var entity = await _context.Aulas
+        var entity = await _context.Certificados
             .FindAsync(new object[] { request.Id }, cancellationToken);
 
         Guard.Against.NotFound(request.Id, entity);
 
-        _context.Aulas.Remove(entity);
+        _context.Certificados.Remove(entity);
 
         var result = await _context.SaveChangesAsync(cancellationToken);
         return result == 1;
