107cd763d984966719d4cf8e45120f26a36013e5
Nacelio.Code <nacelio.code@gmail.com>
2024-12-30 14:41:59 -0300

    Chore:
    iclusao de summary

--- src/Web/Endpoints/Resposta.cs
+++ src/Web/Endpoints/Resposta.cs
@@ -6,9 +6,17 @@ using DnaBrasilApi.Application.Respostas.Queries;
 using DnaBrasilApi.Application.Respostas.Queries.GetRespostaAll;
 
 namespace DnaBrasilApi.Web.Endpoints;
-
+/// <summary>
+/// Api de Resposta
+/// </summary>
 public class Respostas : EndpointGroupBase
 {
+    #region MapEndpoints
+
+    /// <summary>
+    /// Mapeamento dos Endpoints
+    /// </summary>
+    /// <param name="app">Objeto usado para configurar as rotas e os http pipelines</param>
     public override void Map(WebApplication app)
     {
         app.MapGroup(this)
@@ -19,22 +27,28 @@ public class Respostas : EndpointGroupBase
             .MapDelete(DeleteResposta, "{id}")
             .MapGet(GetRespostaById, "Resposta/{id}");
     }
+    #endregion
 
-    public async Task<List<RespostaDto>> GetRespostasAll(ISender sender)
-    {
-        return await sender.Send(new GetRespostasAllQuery());
-    }
+    #region Main Methods
 
-    public async Task<RespostaDto> GetRespostaById(ISender sender, int id)
-    {
-        return await sender.Send(new GetRespostaByIdQuery() { Id = id });
-    }
-
+    /// <summary>
+    /// Endpoint para inclusão de Resposta
+    /// </summary>
+    /// <param name="sender">Sender</param>
+    /// <param name="command">Objeto de inclusão de Resposta</param>
+    /// <returns>Retorna Id de nova Resposta</returns>
     public async Task<int> CreateResposta(ISender sender, CreateRespostaCommand command)
     {
         return await sender.Send(command);
     }
 
+    /// <summary>
+    /// Endpoint para alteração de Resposta
+    /// </summary>
+    /// <param name="sender">Sender</param>
+    /// <param name="id">Id de alteração de Resposta</param>
+    /// <param name="command">Objeto de alteração de Resposta</param>
+    /// <returns>Retorna true ou false</returns>
     public async Task<bool> UpdateResposta(ISender sender, int id, UpdateRespostaCommand command)
     {
         if (id != command.Id) return false;
@@ -42,8 +56,39 @@ public class Respostas : EndpointGroupBase
         return result;
     }
 
+    /// <summary>
+    /// Endpoint para exclusão de Resposta
+    /// </summary>
+    /// <param name="sender">Sender</param>
+    /// <param name="id">Id de exclusao de Resposta</param>
+    /// <returns>Retorna true ou false</returns>
     public async Task<bool> DeleteResposta(ISender sender, int id)
     {
         return await sender.Send(new DeleteRespostaCommand(id));
     }
+    #endregion
+
+    #region Get Methods
+
+    /// <summary>
+    /// Endpoint que busca todas as Resposta cadastradas
+    /// </summary>
+    /// <param name="sender">Sender</param>
+    /// <returns>Retorna a lista de Resposta</returns>
+    public async Task<List<RespostaDto>> GetRespostasAll(ISender sender)
+    {
+        return await sender.Send(new GetRespostasAllQuery());
+    }
+
+    /// <summary>
+    /// Endpoint que busca uma única Resposta
+    /// </summary>
+    /// <param name="sender">Sender</param>
+    /// <param name="id">Id de Resposta a ser buscada</param>
+    /// <returns>Retorna o objeto de Resposta </returns>
+    public async Task<RespostaDto> GetRespostaById(ISender sender, int id)
+    {
+        return await sender.Send(new GetRespostaByIdQuery() { Id = id });
+    }
+    #endregion
 }
