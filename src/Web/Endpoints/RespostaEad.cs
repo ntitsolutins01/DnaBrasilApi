a24d347f717a6c8148790accdecbdae0305f63d0
Nacelio.Code <nacelio.code@gmail.com>
2024-12-30 14:51:58 -0300

    Chore:
    inclusao de summary

--- src/Web/Endpoints/RespostaEad.cs
+++ src/Web/Endpoints/RespostaEad.cs
@@ -6,9 +6,17 @@ using DnaBrasilApi.Application.RespostasEad.Queries.GetRespostaEadById;
 using DnaBrasilApi.Application.RespostasEad.Queries.GetRespostasEadAll;
 
 namespace DnaBrasilApi.Web.Endpoints;
-
+/// <summary>
+/// Api de Resposta Ead
+/// </summary>
 public class RespostasEad : EndpointGroupBase
 {
+    #region MapEndpoints
+
+    /// <summary>
+    /// Mapeamento dos Endpoints
+    /// </summary>
+    /// <param name="app">Objeto usado para configurar as rotas e os http pipelines</param>
     public override void Map(WebApplication app)
     {
         app.MapGroup(this)
@@ -19,22 +27,28 @@ public class RespostasEad : EndpointGroupBase
             .MapDelete(DeleteRespostaEad, "{id}")
             .MapGet(GetRespostaEadById, "RespostaEad/{id}");
     }
+    #endregion
 
-    public async Task<List<RespostaEadDto>> GetRespostasEadAll(ISender sender)
-    {
-        return await sender.Send(new GetRespostasEadAllQuery());
-    }
+    #region Main Methods
 
-    public async Task<RespostaEadDto> GetRespostaEadById(ISender sender, int id)
-    {
-        return await sender.Send(new GetRespostaEadByIdQuery() { Id = id });
-    }
-
+    /// <summary>
+    /// Endpoint para inclusão de Resposta Ead
+    /// </summary>
+    /// <param name="sender">Sender</param>
+    /// <param name="command">Objeto de inclusão de Resposta Ead</param>
+    /// <returns>Retorna Id de nova Resposta Ead</returns>
     public async Task<int> CreateRespostaEad(ISender sender, CreateRespostaEadCommand command)
     {
         return await sender.Send(command);
     }
 
+    /// <summary>
+    /// Endpoint para alteração de Resposta Ead
+    /// </summary>
+    /// <param name="sender">Sender</param>
+    /// <param name="id">Id de alteração de Resposta Ead</param>
+    /// <param name="command">Objeto de alteração de Resposta Ead</param>
+    /// <returns>Retorna true ou false</returns>
     public async Task<bool> UpdateRespostaEad(ISender sender, int id, UpdateRespostaEadCommand command)
     {
         if (id != command.Id) return false;
@@ -42,8 +56,39 @@ public class RespostasEad : EndpointGroupBase
         return result;
     }
 
+    /// <summary>
+    /// Endpoint para exclusão de Resposta Ead
+    /// </summary>
+    /// <param name="sender">Sender</param>
+    /// <param name="id">Id de exclusao de Resposta Ead</param>
+    /// <returns>Retorna true ou false</returns>
     public async Task<bool> DeleteRespostaEad(ISender sender, int id)
     {
         return await sender.Send(new DeleteRespostaEadCommand(id));
     }
+    #endregion
+
+    #region Get Methods
+
+    /// <summary>
+    /// Endpoint que busca todas as Resposta Ead cadastradas
+    /// </summary>
+    /// <param name="sender">Sender</param>
+    /// <returns>Retorna a lista de Resposta Ead</returns>
+    public async Task<List<RespostaEadDto>> GetRespostasEadAll(ISender sender)
+    {
+        return await sender.Send(new GetRespostasEadAllQuery());
+    }
+
+    /// <summary>
+    /// Endpoint que busca uma única Resposta Ead
+    /// </summary>
+    /// <param name="sender">Sender</param>
+    /// <param name="id">Id de Resposta Ead a ser buscada</param>
+    /// <returns>Retorna o objeto de Resposta Ead </returns>
+    public async Task<RespostaEadDto> GetRespostaEadById(ISender sender, int id)
+    {
+        return await sender.Send(new GetRespostaEadByIdQuery() { Id = id });
+    }
+    #endregion
 }
