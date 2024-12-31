c18eba40ac11d81f1bd85d5b68325b2b5e11d179
Nacelio.Code <nacelio.code@gmail.com>
2024-12-30 22:04:30 -0300

    Chore:
    inclusao de summary

--- src/Web/Endpoints/TextosQuestoes.cs
+++ src/Web/Endpoints/TextosQuestoes.cs
@@ -6,9 +6,17 @@ using DnaBrasilApi.Application.TextosQuestoes.Queries.GetTextoQuestaoById;
 using DnaBrasilApi.Application.TextosQuestoes.Queries.GetTextosQuestoesAll;
 
 namespace DnaBrasilApi.Web.Endpoints;
-
+/// <summary>
+/// Api de Texto Questões
+/// </summary>
 public class TextosQuestoes : EndpointGroupBase
 {
+    #region MapEndpoints
+
+    /// <summary>
+    /// Mapeamento dos Endpoints
+    /// </summary>
+    /// <param name="app">Objeto usado para configurar as rotas e os http pipelines</param>
     public override void Map(WebApplication app)
     {
         app.MapGroup(this)
@@ -19,21 +27,28 @@ public class TextosQuestoes : EndpointGroupBase
             .MapDelete(DeleteTextoQuestao, "{id}")
             .MapGet(GetTextoQuestaoById, "TextoQuestao/{id}");
     }
+    #endregion
 
-    public async Task<List<TextoQuestaoDto>> GetTextosQuestoesAll(ISender sender)
-    {
-        return await sender.Send(new GetTextosQuestoesAllQuery());
-    }
+    #region Main Methods
 
-    public async Task<TextoQuestaoDto> GetTextoQuestaoById(ISender sender, int id)
-    {
-        return await sender.Send(new GetTextoQuestaoByIdQuery() { Id = id });
-    }
+    /// <summary>
+    /// Endpoint para inclusão de Texto Questão
+    /// </summary>
+    /// <param name="sender">Sender</param>
+    /// <param name="command">Objeto de inclusão de Texto Questão</param>
+    /// <returns>Retorna Id de novo Texto Questão</returns>
     public async Task<int> CreateTextoQuestao(ISender sender, CreateTextoQuestaoCommand command)
     {
         return await sender.Send(command);
     }
 
+    /// <summary>
+    /// Endpoint para alteração de Texto Questão
+    /// </summary>
+    /// <param name="sender">Sender</param>
+    /// <param name="id">Id de alteração de Texto Questão</param>
+    /// <param name="command">Objeto de alteração de Texto Questão</param>
+    /// <returns>Retorna true ou false</returns>
     public async Task<bool> UpdateTextoQuestao(ISender sender, int id, UpdateTextoQuestaoCommand command)
     {
         if (id != command.Id) return false;
@@ -41,8 +56,39 @@ public class TextosQuestoes : EndpointGroupBase
         return result;
     }
 
+    /// <summary>
+    /// Endpoint para exclusão de Texto Questão
+    /// </summary>
+    /// <param name="sender">Sender</param>
+    /// <param name="id">Id de Exclusão de Texto Questão</param>
+    /// <returns>Retorna true ou false</returns>
     public async Task<bool> DeleteTextoQuestao(ISender sender, int id)
     {
         return await sender.Send(new DeleteTextoQuestaoCommand(id));
     }
+    #endregion
+
+    #region Get Methods
+
+    /// <summary>
+    /// Endpoint que busca todas os Texto Questão cadastradas
+    /// </summary>
+    /// <param name="sender">Sender</param>
+    /// <returns>Retorna a lista de Texto Questão</returns>
+    public async Task<List<TextoQuestaoDto>> GetTextosQuestoesAll(ISender sender)
+    {
+        return await sender.Send(new GetTextosQuestoesAllQuery());
+    }
+
+    /// <summary>
+    /// Endpoint que busca um único Texto Questão
+    /// </summary>
+    /// <param name="sender">Sender</param>
+    /// <param name="id">Id de Texto Questão a ser buscado</param>
+    /// <returns>Retorna o objeto de Texto Questão </returns>
+    public async Task<TextoQuestaoDto> GetTextoQuestaoById(ISender sender, int id)
+    {
+        return await sender.Send(new GetTextoQuestaoByIdQuery() { Id = id });
+    }
+    #endregion
 }
