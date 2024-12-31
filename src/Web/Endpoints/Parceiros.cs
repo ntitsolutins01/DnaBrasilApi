78b36a98f1aa51b245ecb5ec4e79271c50302c97
Nacelio.Code <nacelio.code@gmail.com>
2024-12-31 01:08:20 -0300

    Chore:
    inclusao de summary

--- src/Web/Endpoints/Parceiros.cs
+++ src/Web/Endpoints/Parceiros.cs
@@ -13,6 +13,11 @@ namespace DnaBrasilApi.Web.Endpoints;
 public class Parceiros : EndpointGroupBase
 {
     #region MapEndpoints
+
+    /// <summary>
+    /// Mapeamento dos Endpoints
+    /// </summary>
+    /// <param name="app">Objeto usado para configurar as rotas e os http pipelines</param>
     public override void Map(WebApplication app)
     {
         app.MapGroup(this)
@@ -28,34 +33,24 @@ public class Parceiros : EndpointGroupBase
 
     #region Main Methods
 
-
-    #endregion
-
-    #region Get Methods
-
-
-    #endregion
-
-    public async Task<List<ParceiroDto>> GetParceirosAll(ISender sender)
-    {
-        return await sender.Send(new GetParceirosAllQuery());
-    }
-
-    public async Task<ParceiroDto> GetParceiroById(ISender sender, int id)
-    {
-        return await sender.Send(new GetParceiroByIdQuery() { Id = id });
-    }
-
-    public async Task<ParceiroDto> GetParceiroByAspNetUserId(ISender sender, string aspNetUserId)
-    {
-        return await sender.Send(new GetParceiroByAspNetUserIdQuery() { AspNetUserId = aspNetUserId });
-    }
-
+    /// <summary>
+    /// Endpoint para inclusão de Parceiro
+    /// </summary>
+    /// <param name="sender">Sender</param>
+    /// <param name="command">Objeto de inclusão de Parceiro</param>
+    /// <returns>Retorna Id de novo Parceiro</returns>
     public async Task<int> CreateParceiro(ISender sender, CreateParceiroCommand command)
     {
         return await sender.Send(command);
     }
 
+    /// <summary>
+    /// Endpoint para alteração de Parceiro
+    /// </summary>
+    /// <param name="sender">Sender</param>
+    /// <param name="id">Id de alteração de Parceiro</param>
+    /// <param name="command">Objeto de alteração de Parceiro</param>
+    /// <returns>Retorna true ou false</returns>
     public async Task<bool> UpdateParceiro(ISender sender, int id, UpdateParceiroCommand command)
     {
         if (id != command.Id) return false;
@@ -63,8 +58,50 @@ public class Parceiros : EndpointGroupBase
         return result;
     }
 
+    /// <summary>
+    /// Endpoint para exclusão de Parceiro
+    /// </summary>
+    /// <param name="sender">Sender</param>
+    /// <param name="id">Id de exclusão de Parceiro</param>
+    /// <returns>Retorna true ou false</returns>
     public async Task<bool> DeleteParceiro(ISender sender, int id)
     {
         return await sender.Send(new DeleteParceiroCommand(id));
     }
+    #endregion
+
+    #region Get Methods
+
+    /// <summary>
+    /// Endpoint que busca todos os Parceiros cadastrados
+    /// </summary>
+    /// <param name="sender">Sender</param>
+    /// <returns>Retorna a lista de Parceiros</returns>
+    public async Task<List<ParceiroDto>> GetParceirosAll(ISender sender)
+    {
+        return await sender.Send(new GetParceirosAllQuery());
+    }
+
+    /// <summary>
+    /// Endpoint que busca um único Parceiro
+    /// </summary>
+    /// <param name="sender">Sender</param>
+    /// <param name="id">Id de Parceiro a ser buscado</param>
+    /// <returns>Retorna o objeto de Parceiro </returns>
+    public async Task<ParceiroDto> GetParceiroById(ISender sender, int id)
+    {
+        return await sender.Send(new GetParceiroByIdQuery() { Id = id });
+    }
+
+    /// <summary>
+    /// Endpoint que busca Parceiro por id de Usuário 
+    /// </summary>
+    /// <param name="sender">sender</param>
+    /// <param name="aspNetUserId">aspNetUserId</param>
+    /// <returns></returns>
+    public async Task<ParceiroDto> GetParceiroByAspNetUserId(ISender sender, string aspNetUserId)
+    {
+        return await sender.Send(new GetParceiroByAspNetUserIdQuery() { AspNetUserId = aspNetUserId });
+    }
+    #endregion
 }
