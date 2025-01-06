cc35c83f8b832eb55dbf81b0ed901c78a1f1308c
Fabio Muniz Developer <fabio.muniz01@gmail.com>
2025-01-06 15:06:09 -0300

    chore: migracao profissionalmodalidades

--- src/Domain/Entities/ProfissionalModalidade.cs
+++ src/Domain/Entities/ProfissionalModalidade.cs
@@ -1,5 +1,5 @@
 ﻿namespace DnaBrasilApi.Domain.Entities;
-public class ProfissionalModalidade : BaseAuditableEntity
+public class ProfissionalModalidade
 {
     public int ProfissionalId { get; set; }
     public int ModalidadeId { get; set; }
