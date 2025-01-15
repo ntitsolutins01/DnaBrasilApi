6416ff4ea6d9cdb870fe57cafc9ceb6738136e32
Lutercio Jackson <luterciofilho11@hotmail.com>
2025-01-14 21:38:18 -0300

    chore: add max para Nome da Foto

--- src/Application/Certificados/Commands/UpdateCertificado/UpdateCertificadoCommandValidator.cs
+++ src/Application/Certificados/Commands/UpdateCertificado/UpdateCertificadoCommandValidator.cs
@@ -11,5 +11,11 @@ internal class UpdateCertificadoCommandValidator : AbstractValidator<UpdateCerti
             .MaximumLength(2000)
             .NotEmpty()
             .WithMessage("O html é obrigatório.");
+        RuleFor(v => v.NomeFotoFrente)
+            .MaximumLength(100)
+            .WithMessage("O tamanho máximo é 100.");
+        RuleFor(v => v.NomeFotoVerso)
+            .MaximumLength(100)
+            .WithMessage("O tamanho máximo é 100.");
     }
 }
