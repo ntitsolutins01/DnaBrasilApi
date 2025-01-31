668c5c528cc6b4a98fc09d72fdd213d1c05d0f3d
Fabio Muniz Developer <fabio.muniz01@gmail.com>
2025-01-30 23:58:14 -0300

    chore: mudança de nome da tabela

--- src/Infrastructure/Data/ApplicationDbContext.cs
+++ src/Infrastructure/Data/ApplicationDbContext.cs
@@ -78,7 +78,7 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>, IApplica
     public DbSet<AlunoModalidade> AlunoModalidades => Set<AlunoModalidade>();
     public DbSet<Ranking> Rankings => Set<Ranking>();
     public DbSet<AtividadeAluno> AtividadeAlunos => Set<AtividadeAluno>();
-    public DbSet<NivelEscolaridade> NiveisEscolaridade => Set<NivelEscolaridade>();
+    public DbSet<EtapaEnsino> EtapasEnsino => Set<EtapaEnsino>();
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
