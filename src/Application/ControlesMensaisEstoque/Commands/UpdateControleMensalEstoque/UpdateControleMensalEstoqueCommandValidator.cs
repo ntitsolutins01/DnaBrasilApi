748f2da360e55501c874f9dd7e8eea503f84709e
Lutercio Jackson <luterciofilho11@hotmail.com>
2025-02-15 11:55:30 -0300

    chore: update Estoque/Saida

--- src/Application/ControlesMensaisEstoque/Commands/UpdateControleMensalEstoque/UpdateControleMensalEstoqueCommandValidator.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DnaBrasilApi.Application.ControlesMensaisEstoque.Commands.UpdateControleMensalEstoque;
-internal class UpdateControleMensalEstoqueCommandValidator : AbstractValidator<UpdateControleMensalEstoqueCommand>
-{
-    public UpdateControleMensalEstoqueCommandValidator()
-    {
-        RuleFor(v => v.JustificativaDanificadosExtraviados)
-            .MaximumLength(250);
-    }
-}
