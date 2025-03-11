d29b0407fc5a948205d363fb9dc4f77b8211ca96
Fabio Muniz Developer <fabio.muniz01@gmail.com>
2025-03-11 09:27:33 -0300

    chore: implementação de backend series

--- src/Application/Series/Commands/UpdateSerie/UpdateSerie.cs
+++ src/Application/Series/Commands/UpdateSerie/UpdateSerie.cs
@@ -1,52 +1,52 @@
-﻿//using DnaBrasilApi.Application.Common.Interfaces;
-//using DnaBrasilApi.Domain.Entities;
+﻿using DnaBrasilApi.Application.Common.Interfaces;
+using DnaBrasilApi.Domain.Entities;
 
-//namespace DnaBrasilApi.Application.Series.Commands.UpdateSerie;
+namespace DnaBrasilApi.Application.Series.Commands.UpdateSerie;
 
-//public record UpdateSerieCommand : IRequest <bool>
-//{
-//    public int Id { get; init; }
-//    public required string Nome { get; init; }
-//    public required string Turma { get; init; }
-//    public required int EtapaEnsinoId { get; init; }
-//    public required int LocalidadeId { get; init; }
-//    public bool Status { get; init; }
-//}
+public record UpdateSerieCommand : IRequest<bool>
+{
+    public int Id { get; init; }
+    public required string Nome { get; init; }
+    public required string Turma { get; init; }
+    public required int EtapaEnsinoId { get; init; }
+    public required int LocalidadeId { get; init; }
+    public bool Status { get; init; }
+}
 
-//public class UpdateSerieCommandHandler : IRequestHandler<UpdateSerieCommand, bool>
-//{
-//    private readonly IApplicationDbContext _context;
+public class UpdateSerieCommandHandler : IRequestHandler<UpdateSerieCommand, bool>
+{
+    private readonly IApplicationDbContext _context;
 
-//    public UpdateSerieCommandHandler(IApplicationDbContext context)
-//    {
-//        _context = context;
-//    }
+    public UpdateSerieCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
 
-//    public async Task <bool> Handle(UpdateSerieCommand request, CancellationToken cancellationToken)
-//    {
-//        var entity = await _context.Series
-//            .FindAsync(new object[] { request.Id }, cancellationToken);
+    public async Task<bool> Handle(UpdateSerieCommand request, CancellationToken cancellationToken)
+    {
+        var entity = await _context.Series
+            .FindAsync(new object[] { request.Id }, cancellationToken);
 
-//        Guard.Against.NotFound(request.Id, entity); 
-        
-//        var etapaEnsino = await _context.EtapasEnsino
-//            .FindAsync([request.EtapaEnsinoId], cancellationToken);
+        Guard.Against.NotFound(request.Id, entity);
 
-//        Guard.Against.NotFound(request.EtapaEnsinoId, etapaEnsino);
+        var etapaEnsino = await _context.EtapasEnsino
+            .FindAsync([request.EtapaEnsinoId], cancellationToken);
 
-//        var localidade = await _context.Localidades
-//            .FindAsync([request.LocalidadeId], cancellationToken);
+        Guard.Against.NotFound(request.EtapaEnsinoId, etapaEnsino);
 
-//        Guard.Against.NotFound(request.LocalidadeId, localidade);
+        var localidade = await _context.Localidades
+            .FindAsync([request.LocalidadeId], cancellationToken);
 
-//        entity.Nome = request.Nome;
-//        entity.Turma = request.Nome;
-//        entity.EtapaEnsino = etapaEnsino;
-//        entity.Localidade = localidade;
-//        entity.Status = request.Status;
+        Guard.Against.NotFound(request.LocalidadeId, localidade);
 
-//        var result = await _context.SaveChangesAsync(cancellationToken);
+        entity.Nome = request.Nome;
+        entity.Turma = request.Nome;
+        entity.EtapaEnsino = etapaEnsino;
+        entity.Localidade = localidade;
+        entity.Status = request.Status;
 
-//        return result == 1;//true
-//    }
-//}
+        var result = await _context.SaveChangesAsync(cancellationToken);
+
+        return result == 1;//true
+    }
+}
--- src/Application/Series/Queries/SerieDto.cs
+++ src/Application/Series/Queries/SerieDto.cs
@@ -1,21 +1,21 @@
-﻿//using DnaBrasilApi.Domain.Entities;
+﻿using DnaBrasilApi.Domain.Entities;
 
-//namespace DnaBrasilApi.Application.Series.Queries;
-//public class SerieDto
-//{
-//    public int Id { get; init; }
-//    public string? Nome { get; init; }
-//    public string? Descricao { get; init; }
-//    public  int IdadeInicial { get; set; }
-//    public  int IdadeFinal { get; set; }
-//    public  int ScoreTotal { get; set; }
-//    public bool Status { get; set; }
+namespace DnaBrasilApi.Application.Series.Queries;
+public class SerieDto
+{
+    public int Id { get; init; }
+    public required string Nome { get; set; }
+    public required string Turma { get; set; }
+    public required string NomeEtapaEnsino { get; set; }
+    public required int LocalidadeId { get; set; }
 
-//    private class Mapping : Profile
-//    {
-//        public Mapping()
-//        {
-//            CreateMap<Serie, SerieDto>();
-//        }
-//    }
-//}
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<Serie, SerieDto>()
+                .ForMember(dest => dest.NomeEtapaEnsino, opt => opt.MapFrom(src => src.EtapaEnsino.Nome))
+                .ForMember(dest => dest.LocalidadeId, opt => opt.MapFrom(src => src.Localidade.Id));
+        }
+    }
+}
