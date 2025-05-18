9449d0eef244ea7190c7b7ac57185ea91a085b12
Fabio Muniz Developer <fabio.muniz01@gmail.com>
2025-05-18 20:35:59 -0300

    chore: migraçao + endpoints

---
+++ src/Application/Laudos/Queries/EducacionalDto.cs
@@ -0,0 +1,25 @@
+﻿using DnaBrasilApi.Application.Alunos.Queries;
+using DnaBrasilApi.Application.Encaminhamentos.Queries;
+using DnaBrasilApi.Application.Profissionais.Queries;
+using DnaBrasilApi.Application.Series.Queries;
+using DnaBrasilApi.Domain.Entities;
+
+namespace DnaBrasilApi.Application.Laudos.Queries;
+public class EducacionalDto
+{
+    public int Id { get; init; }
+    public required ProfissionalDto Profissional { get; init; }
+    public required AlunoDto Aluno { get; init; }
+    public required SerieDto Serie { get; init; }
+    public required string Pdf { get; init; }
+    public required string Respostas { get; init; }
+    public EncaminhamentoDto? Encaminhamento { get; init; }
+    public string? StatusEducacional { get; init; }
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<Educacional, EducacionalDto>();
+        }
+    }
+}
