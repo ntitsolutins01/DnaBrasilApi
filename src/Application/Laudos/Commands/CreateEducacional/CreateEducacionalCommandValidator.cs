9f44987da642bffe5ac44333a520af07c6c8cbb3
Fabio Muniz Developer <fabio.muniz01@gmail.com>
2025-05-18 20:10:37 -0300

    chore: crud de educacional

---
+++ src/Application/Laudos/Commands/CreateEducacional/CreateEducacionalCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace DnaBrasilApi.Application.Laudos.Commands.CreateVocacional;
+
+public class CreateVocacionalCommandValidator : AbstractValidator<CreateVocacionalCommand>
+{
+    public CreateVocacionalCommandValidator()
+    {
+        RuleFor(v => v.Respostas)
+            .MaximumLength(500)
+            .NotEmpty();
+    }
+}
