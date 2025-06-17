8f4bc186febaef292bbb30315b6edb2ea0f1c493
Fabio Muniz Developer <fabio.muniz01@gmail.com>
2025-06-17 15:59:21 -0300

    chore: exclusao de fomentolocalidades

--- src/Application/Localidades/Commands/DeleteLocalidade/DeleteLocalidade.cs
+++ src/Application/Localidades/Commands/DeleteLocalidade/DeleteLocalidade.cs
@@ -24,6 +24,17 @@ public class DeleteLocalidadeCommandHandler : IRequestHandler<DeleteLocalidadeCo
 
         Guard.Against.PossuiProfissionais(possuiProfissionais);
 
+        var possuiAlunos = _context.Alunos.Any(x => x.Localidade != null && x.Localidade.Id == request.Id);
+
+        Guard.Against.PossuiAlunosLocalidades(possuiAlunos);
+
+        var list = await _context.FomentoLocalidades
+            .Where(x => x.LocalidadeId == request.Id)
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
+
+        _context.FomentoLocalidades.RemoveRange(list);
+
         _context.Localidades.Remove(entity);
 
         var result = await _context.SaveChangesAsync(cancellationToken);
