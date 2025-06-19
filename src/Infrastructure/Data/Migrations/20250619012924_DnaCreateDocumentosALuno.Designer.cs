7f2031838824ff795c2207dbe54488e46e10ea99
Fabio Muniz Developer <fabio.muniz01@gmail.com>
2025-06-18 22:40:40 -0300

    chore: Criação de endpoint para inclusao de documentos do aluno

--- src/Infrastructure/Data/Migrations/20250619012924_DnaCreateDocumentosALuno.Designer.cs
+++ src/Infrastructure/Data/Migrations/20250619012924_DnaCreateDocumentosALuno.Designer.cs
@@ -4,6 +4,7 @@ using DnaBrasilApi.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DnaBrasilApi.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250619012924_DnaCreateDocumentosALuno")]
+    partial class DnaCreateDocumentosALuno
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -976,16 +979,14 @@ namespace DnaBrasilApi.Infrastructure.Data.Migrations
 
                     b.Property<string>("NomeDocumento")
                         .IsRequired()
-                        .HasMaxLength(250)
-                        .HasColumnType("nvarchar(250)");
+                        .HasColumnType("nvarchar(max)");
 
                     b.Property<bool>("Status")
                         .HasColumnType("bit");
 
                     b.Property<string>("Url")
                         .IsRequired()
-                        .HasMaxLength(500)
-                        .HasColumnType("nvarchar(500)");
+                        .HasColumnType("nvarchar(max)");
 
                     b.HasKey("Id");
 
