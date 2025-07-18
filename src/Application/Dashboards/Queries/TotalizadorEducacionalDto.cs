52b79ab49114898c147fdadc55ac852af6f992a5
Fabio Muniz Developer <fabio.muniz01@gmail.com>
2025-07-17 23:32:16 -0300

    chore: v2.2.5 - Implementação de Totalizador Educacional para o Dashboard

---
+++ src/Application/Dashboards/Queries/TotalizadorEducacionalDto.cs
@@ -0,0 +1,9 @@
+﻿namespace DnaBrasilApi.Application.Dashboards.Queries;
+public class TotalizadorEducacionalDto
+{
+    public Dictionary<string, decimal>? PercTotalizadorEducacionalMasculino { get; set; }
+    public Dictionary<string, decimal>? PercTotalizadorEducacionalFeminino { get; set; }
+    public Dictionary<string, decimal>? ValorTotalizadorEducacionalMasculino { get; set; }
+    public Dictionary<string, decimal>? ValorTotalizadorEducacionalFeminino { get; set; }
+    public Dictionary<string, decimal>? PercentualEducacional { get; set; }
+}
