6e38781d721aca5bd5f8fd1785bcbfe5e865fd75
Fabio Muniz Developer <fabio.muniz01@gmail.com>
2025-07-24 13:23:43 -0300

    chore: alteração de filttro pra busca de indicadores

--- src/Application/Cursos/Queries/GetQuantidadeCursosByProgresso/GetQuantidadeCursosByProgresso.cs
+++ src/Application/Cursos/Queries/GetQuantidadeCursosByProgresso/GetQuantidadeCursosByProgresso.cs
@@ -7,6 +7,7 @@ public record GetQuantidadeCursosByProgressoQuery : IRequest<int>
     public required int ProgressoIni { get; init; }
     public required int ProgressoFim { get; init; }
     public string? CursoId { get; init; }
+    public string? TipoCursoId { get; init; }
 };
 
 public class GetQuantidadeCursosByProgressoQueryHandler : IRequestHandler<GetQuantidadeCursosByProgressoQuery, int>
@@ -23,17 +24,21 @@ public class GetQuantidadeCursosByProgressoQueryHandler : IRequestHandler<GetQua
     public async Task<int> Handle(GetQuantidadeCursosByProgressoQuery request, CancellationToken cancellationToken)
     {
         int result;
-
-        result = request.CursoId == null
-            ? await _context.AlunoCursosCertificados
-                .Where(x => x.Progresso > request.ProgressoIni && x.Progresso < request.ProgressoFim)
+        if (request.CursoId == null && request.TipoCursoId != null)
+        {
+            result = await _context.AlunoCursosCertificados
+                .Where(x => x.Progresso > request.ProgressoIni && x.Progresso < request.ProgressoFim && request.TipoCursoId == x.Curso!.TipoCurso.Id.ToString())
                 .AsNoTracking()
-                .CountAsync()
-            : await _context.AlunoCursosCertificados
+                .CountAsync();
+        }
+        else
+        {
+            result = await _context.AlunoCursosCertificados
                 .Where(x => x.Progresso > request.ProgressoIni && x.Progresso < request.ProgressoFim &&
                             x.CursoId == Convert.ToInt32(request.CursoId))
                 .AsNoTracking()
                 .CountAsync();
+        }
 
         return result;
     }
