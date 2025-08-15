620aeca7c55c262af28f8a23df3efcba2eb7246c
Lutercio Jackson <luterciofilho11@hotmail.com>
2025-08-15 18:20:46 -0300

    chore: implementação das rotas da ApiPython

--- src/Application/Laudos/Queries/ProcessarGabarito/ProcessarGabarito.cs
+++ src/Application/Laudos/Queries/ProcessarGabarito/ProcessarGabarito.cs
@@ -1,9 +1,5 @@
-﻿using System.Net.Http;
-using System.Net.Http.Headers;
+﻿using System.Text;
 using System.Text.Json;
-using Ardalis.GuardClauses;
-using DnaBrasilApi.Application.Common.Interfaces;
-using DnaBrasilApi.Domain.Entities;
 
 namespace DnaBrasilApi.Application.Laudos.Queries.ProcessarGabarito;
 
@@ -12,38 +8,25 @@ public record ProcessarGabaritoCommand : IRequest<Dictionary<string, object>>
     public byte[]? ByteImage { get; init; }
 }
 
-public class ProcessarGabaritoCommandHandler : IRequestHandler<ProcessarGabaritoCommand, Dictionary<string, object>>
+public class ProcessarGabaritoCommandHandler
+    : IRequestHandler<ProcessarGabaritoCommand, Dictionary<string, object>>
 {
-    private readonly IApplicationDbContext _context;
-    private readonly HttpClient _httpClient = new HttpClient();
+    private readonly HttpClient _httpClient;
 
-    public ProcessarGabaritoCommandHandler(IApplicationDbContext context)
+    public ProcessarGabaritoCommandHandler(IHttpClientFactory httpClientFactory)
     {
-        _context = context;
+        _httpClient = httpClientFactory.CreateClient("PythonApi");
     }
 
     public async Task<Dictionary<string, object>> Handle(ProcessarGabaritoCommand request, CancellationToken cancellationToken)
     {
-        if (request.ByteImage is null)
-            throw new Exception("Imagem não fornecida.");
+        var payload = new { image = Convert.ToBase64String(request.ByteImage!) };
+        var content = new StringContent(JsonSerializer.Serialize(payload), Encoding.UTF8, "application/json");
 
-        var base64Image = Convert.ToBase64String(request.ByteImage);
-
-        var body = new { image = base64Image };
-
-        var jsonContent = new StringContent(
-            JsonSerializer.Serialize(body),
-            System.Text.Encoding.UTF8,
-            "application/json"
-        );
-
-        var response = await _httpClient.PostAsync("http://localhost:5050/corrigir", jsonContent, cancellationToken);
+        var response = await _httpClient.PostAsync("/corrigir", content, cancellationToken);
         response.EnsureSuccessStatusCode();
 
         var json = await response.Content.ReadAsStringAsync(cancellationToken);
-
-        var data = JsonSerializer.Deserialize<Dictionary<string, object>>(json)!;
-
-        return data;
+        return JsonSerializer.Deserialize<Dictionary<string, object>>(json)!;
     }
 }
