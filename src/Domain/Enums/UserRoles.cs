821151a16dde0289a3693f71c46ca9aba1d13d6e
Fabio Muniz Developer <fabio.muniz01@gmail.com>
2025-10-03 15:55:27 -0300

    chore: correção para cadastro de evento

--- src/Domain/Enums/UserRoles.cs
+++ src/Domain/Enums/UserRoles.cs
@@ -12,4 +12,6 @@ public static class UserRoles
     public const string ProfessorEad = "ProfessorEad";
     public const string CoordenadorEad = "CoordenadorEad";
     public const string Coordenador = "Coordenador Pedagógico";
+    public const string CoordenadorGeral = "Coordenador Geral";
+    public const string EquipeItinerante = "Equipe Itinerante";
 }
--- src/Web/Endpoints/EtapasEnsino.cs
+++ src/Web/Endpoints/EtapasEnsino.cs
@@ -1,6 +1,10 @@
-﻿using DnaBrasilApi.Application.EtapasEnsino.Queries;
+﻿using DnaBrasilApi.Application.EtapasEnsino.Commands.CreateEtapaEnsino;
+using DnaBrasilApi.Application.EtapasEnsino.Commands.DeleteEtapaEnsino;
+using DnaBrasilApi.Application.EtapasEnsino.Commands.UpdateEtapaEnsino;
+using DnaBrasilApi.Application.EtapasEnsino.Queries;
 using DnaBrasilApi.Application.EtapasEnsino.Queries.GetEtapaEnsinoById;
 using DnaBrasilApi.Application.EtapasEnsino.Queries.GetEtapasEnsinoAll;
+using DnaBrasilApi.Application.EtapasEnsino.Queries.GetEtapasEnsinoAtivas;
 
 namespace DnaBrasilApi.Web.Endpoints;
 /// <summary>
@@ -19,25 +23,26 @@ public class EtapasEnsino : EndpointGroupBase
         app.MapGroup(this)
             //.RequireAuthorization()
             .MapGet(GetEtapasEnsinoAll)
-            //.MapPost(CreateEtapaEnsino)
-            //.MapPut(UpdateEtapaEnsino, "{id}")
-            //.MapDelete(DeleteEtapaEnsino, "{id}")
+            .MapGet(GetEtapasEnsinoAtivas, "Ativas")
+            .MapPost(CreateEtapaEnsino)
+            .MapPut(UpdateEtapaEnsino, "{id}")
+            .MapDelete(DeleteEtapaEnsino, "{id}")
             .MapGet(GetEtapaEnsinoById, "{id}");
     }
     #endregion
 
     #region Main Methods
 
-    ///// <summary>
-    ///// Endpoint para inclusão de Etapas de Ensino
-    ///// </summary>
-    ///// <param name="sender">Sender</param>
-    ///// <param name="command">Objeto de inclusão de Etapas de Ensino</param>
-    ///// <returns>Retorna Id de nova Etapas de Ensino</returns>
-    //public async Task<int> CreateEtapaEnsino(ISender sender, CreateEtapaEnsinoCommand command)
-    //{
-    //    return await sender.Send(command);
-    //}
+    /// <summary>
+    /// Endpoint para inclusão de Etapas de Ensino
+    /// </summary>
+    /// <param name="sender">Sender</param>
+    /// <param name="command">Objeto de inclusão de Etapas de Ensino</param>
+    /// <returns>Retorna Id de nova Etapas de Ensino</returns>
+    public async Task<int> CreateEtapaEnsino(ISender sender, CreateEtapaEnsinoCommand command)
+    {
+        return await sender.Send(command);
+    }
 
     ///// <summary>
     ///// Endpoint para alteração de Etapas de Ensino
@@ -46,12 +51,12 @@ public class EtapasEnsino : EndpointGroupBase
     ///// <param name="id">Id de alteração de Etapas de Ensino</param>
     ///// <param name="command">Objeto de alteração de Etapas de Ensino</param>
     ///// <returns>Retorna true ou false</returns>
-    //public async Task<bool> UpdateEtapaEnsino(ISender sender, int id, UpdateEtapaEnsinoCommand command)
-    //{
-    //    if (id != command.Id) return false;
-    //    var result = await sender.Send(command);
-    //    return result;
-    //}
+    public async Task<bool> UpdateEtapaEnsino(ISender sender, int id, UpdateEtapaEnsinoCommand command)
+    {
+        if (id != command.Id) return false;
+        var result = await sender.Send(command);
+        return result;
+    }
 
     ///// <summary>
     ///// Endpoint para exclusão de Etapas de Ensino
@@ -59,10 +64,10 @@ public class EtapasEnsino : EndpointGroupBase
     ///// <param name="sender">Sender</param>
     ///// <param name="id">Id de Exclusão de Etapas de Ensino</param>
     ///// <returns>Retorna true ou false</returns>
-    //public async Task<bool> DeleteEtapaEnsino(ISender sender, int id)
-    //{
-    //    return await sender.Send(new DeleteEtapaEnsinoCommand(id));
-    //}
+    public async Task<bool> DeleteEtapaEnsino(ISender sender, int id)
+    {
+        return await sender.Send(new DeleteEtapaEnsinoCommand(id));
+    }
     #endregion
 
     #region Get Methods
@@ -75,6 +80,11 @@ public class EtapasEnsino : EndpointGroupBase
     public async Task<List<EtapaEnsinoDto>> GetEtapasEnsinoAll(ISender sender)
     {
         return await sender.Send(new GetEtapasEnsinoAllQuery());
+    }
+
+    public async Task<List<EtapaEnsinoDto>> GetEtapasEnsinoAtivas(ISender sender)
+    {
+        return await sender.Send(new GetEtapasEnsinoAtivasQuery());
     }
 
     /// <summary>
